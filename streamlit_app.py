ef28f712315c69faf682f724ea5a15010cb55de9
vjonnaladito <132283610+vjonnaladito@users.noreply.github.com>
2023-05-01 23:49:23 +0800

    updated with 4th Item
    Changed the 4th Item

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 &  Blueberry Oatmeal')
+streamlit.text(' 🥣 Omega 3 &  Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('🥗Kale, Spinach & Rocket Smothie')
 
-streamlit.text('Hard-Boiled- Free Range Egg')
+streamlit.text('🐔Hard-Boiled- Free Range Egg')
+
+streamlit.text('🥑🍞Avacado Toast')
