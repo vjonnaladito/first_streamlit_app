4a4a8d51d0b866c9cd16df20c58cc4338d8fe9a6
vjonnaladito <132283610+vjonnaladito@users.noreply.github.com>
2023-05-02 20:35:16 +0800

    calling pandas
    Pandas fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,9 @@ streamlit.text('🥑🍞Avacado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
